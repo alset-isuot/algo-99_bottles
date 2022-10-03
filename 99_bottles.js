bbe656d93c9dd232c73f784e8796f11c47b08ed1
Tesla.Tousi <80373713+alset-isuot@users.noreply.github.com>
2022-10-03 16:16:52 -0500

    Add files via upload

---
+++ 99_bottles.js
@@ -0,0 +1,38 @@
+function bottleSong() {
+  // Write your code here!
+  let bottlesStr = 'bottles';
+
+  let startBottleCount = 3;
+  let CurrentBottleCountStrt = startBottleCount;
+  let CurrentBottleCountEnd = startBottleCount -1;
+  
+  let repeatPhrase1 = 'of beer on the wall';
+  let repeatPhrase2 = 'Take one down and pass it around';
+  
+  
+  let endSongVerse = repeatPhrase2 +', no more bottles ' + repeatPhrase1 +'. No more bottles '+repeatPhrase1+ ', no more bottles of beer. Go to the store and buy some more, 99 bottles ' + repeatPhrase1 +'.';
+  /* doesnt dynamically change the values with verse
+  let mainSongVerse = CurrentBottleCount +' '+bottlesStr +' ' + repeatPhrase1+ ', ' + CurrentBottleCount + ' '+bottlesStr+' of beer. ' + repeatPhrase2+ ', '+ CurrentBottleCount +' ' + bottlesStr + ' ' +repeatPhrase1 + '.';
+  */
+  
+  do {
+      // dynamically works, what is suppose to change in each loop does
+      
+      let mainSongVerse = CurrentBottleCountStrt +' '+bottlesStr +' ' + repeatPhrase1+ ', ' + CurrentBottleCountStrt + ' '+bottlesStr+' of beer. ' + repeatPhrase2+ ', '+ CurrentBottleCountEnd +' ' + bottlesStr + ' ' +repeatPhrase1 + '.';
+      console.log(mainSongVerse);
+      CurrentBottleCountStrt --;
+      CurrentBottleCountEnd = CurrentBottleCountStrt -1;
+      //console.log(CurrentBottleCount); //used this to check to see why the mainVerse variable wasn't working as intented
+      if (CurrentBottleCountEnd == 1){
+          bottlesStr = 'bottle';
+      }
+  }
+  while(CurrentBottleCountEnd>0)
+  
+    if (CurrentBottleCountEnd == 0){
+      console.log(endSongVerse);
+    }
+  
+  };
+    
+  bottleSong();
